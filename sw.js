6ac02c0c7caaa0ac4636fce85227f94059cb46c8
kariham <132402525+kariham@users.noreply.github.com>
2026-07-19 00:00:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
-const CACHE = "wpi-cache-v1"; const ASSETS = [ "./", "./index.html", "./manifest.webmanifest", "./styles/main.css", "./js/app.js", "./js/forms.js", "./js/pdf.js", "./js/db.js", "./js/pwa.js", "./assets/logo.png" ];
+const CACHE_NAME = 'wpi-qhse-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles/style.css',
+  './js/main.js',
+  './manifest.webmanifest'
+];
 
-self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS))); });
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
 
-self.addEventListener("activate", (e) => { e.waitUntil( caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))) ) ); });
-
-self.addEventListener("fetch", (e) => { e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request))); });
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+});
