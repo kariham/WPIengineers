9c68f9417d7423a9b093edcc3e68f46b63603b4e
kariham <132402525+kariham@users.noreply.github.com>
2026-07-18 21:01:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
+const CACHE = "wpi-cache-v1"; const ASSETS = [ "./", "./index.html", "./manifest.webmanifest", "./styles/main.css", "./js/app.js", "./js/forms.js", "./js/pdf.js", "./js/db.js", "./js/pwa.js", "./assets/logo.png" ];
 
+self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS))); });
+
+self.addEventListener("activate", (e) => { e.waitUntil( caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))) ) ); });
+
+self.addEventListener("fetch", (e) => { e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request))); });
